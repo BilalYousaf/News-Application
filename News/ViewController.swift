8b32d4b79225343720125efdf9b8695f4fadd982
Salman <salman@Salmans-MacBook-Pro.local>
2018-01-07 15:42:59 +0500

    Initial Commit

---
+++ News/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by Salman on 1/7/18.
+//  Copyright © 2018 cannypope. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
